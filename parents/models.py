e4e3c2f870cce4be880be62a226389765f1fd741
OmarElaraby26 <omarelaraby26@gmail.com>
2022-10-31 07:17:18 +0200

    fix bug: name validator

--- parents/models.py
+++ parents/models.py
@@ -1,11 +1,19 @@
 from django.db import models
+from django.core.validators import RegexValidator
 
 # Create your models here.
 
 
+is_valid_name_regex = RegexValidator(
+    regex="^[A-Z][a-z]+$", message="name must be at least 3 letters and contain only lowercase letters except the first one must be uppercase")
+
+
 class Parent(models.Model):
-    firstname = models.CharField(max_length=32)
-    lastname = models.CharField(max_length=32)
+
+    firstname = models.CharField(
+        max_length=32,  validators=[is_valid_name_regex])
+    lastname = models.CharField(
+        max_length=32,  validators=[is_valid_name_regex])
     age = models.PositiveIntegerField()
 
     def __str__(self):
