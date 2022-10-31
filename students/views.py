0997aa4659f43bd7216ab0b6d7f61cd403826ca2
OmarElaraby26 <omarelaraby26@gmail.com>
2022-10-31 07:20:50 +0200

    convert views to class views (APIView)

--- students/views.py
+++ students/views.py
@@ -1,59 +1,47 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import JsonResponse
+from students.serializers import StudentSerializer
 from .models import Student
-from .forms import StudentForm
-import json
-from django.core import serializers
+from rest_framework.views import APIView
+from django.http import Http404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
 
-def getAllStudents(request):
-    allStudents = serializers.serialize('json', Student.objects.all())
-    return JsonResponse(json.loads(allStudents), safe=False)
+class StudentList(APIView):
+    def get(self, request, format=None):
+        students = Student.objects.all()
+        serializer = StudentSerializer(students, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request, format=None):
+        serializer = StudentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-def createStudent(request):
-    newStudent = StudentForm(request.POST)
-    if newStudent.is_valid():
-        newStudent.save()
-        return JsonResponse(newStudent.data, status=201)
-    else:
-        return JsonResponse(newStudent.errors, status=409)
+class StudentDetail(APIView):
+    def get_object(self, id):
+        try:
+            return Student.objects.get(id=id)
+        except Student.DoesNotExist:
+            raise Http404
 
+    def get(self, request, id, format=None):
+        student = self.get_object(id)
+        serializer = StudentSerializer(student)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
-def studentsController(request):
-    if request.method == 'GET':
-        return getAllStudents(request)
-    if request.method == 'POST':
-        return createStudent(request)
+    def put(self, request, id, format=None):
+        student = self.get_object(id)
+        serializer = StudentSerializer(student, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-
-def getOneStudent(request, id):
-    data = serializers.serialize('json', Student.objects.all().filter(id=id))
-    return JsonResponse(json.loads(data), safe=False)
-
-
-def updateOneStudent(request, id):
-    obj = get_object_or_404(Student, id=id)
-    student = StudentForm(request.POST, instance=obj)
-    if student.is_valid():
-        student.save()
-        return JsonResponse(student.data)
-    else:
-        return JsonResponse(student.errors, status=400)
-
-
-def deleteOneStudent(request, id):
-    obj = get_object_or_404(Student, id=id)
-    obj.delete()
-    return JsonResponse({'status': 'success'})
-
-
-def oneStudentController(request, id):
-    if request.method == 'GET':
-        return getOneStudent(request, id)
-    elif request.method == 'POST':
-        return updateOneStudent(request, id)
-    elif request.method == 'DELETE':
-        return deleteOneStudent(request, id)
-    else:
-        return JsonResponse({'message:': 'method not allowed'}, status=405)
+    def delete(self, request, id, format=None):
+        snippet = self.get_object(id)
+        snippet.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
