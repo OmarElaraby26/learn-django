86fc6a72280d23c4c4965269c04a18c3f3816167
OmarElaraby26 <omarelaraby26@gmail.com>
2022-10-31 07:17:35 +0200

    fix bug: name validator

--- students/models.py
+++ students/models.py
@@ -1,23 +1,21 @@
 
 from django.db import models
-
 from parents.models import Parent
 from subjects.models import Subject
+from django.core.validators import RegexValidator
 
 # Create your models here.
 
 
-def validateName(name):
-    if not all(x.isalpha for x in name):
-        return False
-    if not all(x.islower for x in name[1::]):
-        return False
-    return name[0].isupper()
+is_valid_name_regex = RegexValidator(
+    regex="^[A-Z][a-z]+$", message="name must be at least 3 letters and contain only lowercase letters except the first one must be uppercase")
 
 
 class Student(models.Model):
-    firstname = models.CharField(max_length=32, validators=[validateName])
-    lastname = models.CharField(max_length=32, validators=[validateName])
+    firstname = models.CharField(
+        max_length=32, validators=[is_valid_name_regex])
+    lastname = models.CharField(
+        max_length=32, validators=[is_valid_name_regex])
     age = models.PositiveIntegerField()
     email = models.EmailField(max_length=255, unique=True)
     year = models.PositiveIntegerField()
