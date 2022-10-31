9c880b2cb5d53cd134e7e8cc4db964b9fdacdbfd
OmarElaraby26 <omarelaraby26@gmail.com>
2022-10-31 07:21:22 +0200

    convert views to class views (mixins)

--- subjects/views.py
+++ subjects/views.py
@@ -1,59 +1,34 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import JsonResponse
+from subjects.serializers import SubjectSerializer
 from .models import Subject
-from .forms import SubjectForm
-import json
-from django.core import serializers
+from rest_framework import mixins
+from rest_framework import generics
 
 
-def getAllSubjects(request):
-    allSubjects = serializers.serialize('json', Subject.objects.all())
-    return JsonResponse(json.loads(allSubjects), safe=False)
+class SubjectList(mixins.ListModelMixin,
+                  mixins.CreateModelMixin,
+                  generics.GenericAPIView):
+    queryset = Subject.objects.all()
+    serializer_class = SubjectSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
 
 
-def createSubject(request):
-    newSubject = SubjectForm(request.POST)
-    if newSubject.is_valid():
-        newSubject.save()
-        return JsonResponse(newSubject.data, status=201)
-    else:
-        return JsonResponse(newSubject.errors, status=409)
+class SubjectDetail(mixins.RetrieveModelMixin,
+                    mixins.UpdateModelMixin,
+                    mixins.DestroyModelMixin,
+                    generics.GenericAPIView):
+    queryset = Subject.objects.all()
+    serializer_class = SubjectSerializer
 
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
 
-def subjectsController(request):
-    if request.method == 'GET':
-        return getAllSubjects(request)
-    if request.method == 'POST':
-        return createSubject(request)
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
 
-
-def getOneSubject(request, id):
-    data = serializers.serialize('json', Subject.objects.all().filter(id=id))
-    return JsonResponse(json.loads(data), safe=False)
-
-
-def updateOneSubject(request, id):
-    obj = get_object_or_404(Subject, id=id)
-    subject = SubjectForm(request.POST, instance=obj)
-    if subject.is_valid():
-        subject.save()
-        return JsonResponse(subject.data)
-    else:
-        return JsonResponse(subject.errors, status=400)
-
-
-def deleteOneSubject(request, id):
-    obj = get_object_or_404(Subject, id=id)
-    obj.delete()
-    return JsonResponse({'status': 'success'})
-
-
-def oneSubjectController(request, id):
-    if request.method == 'GET':
-        return getOneSubject(request, id)
-    elif request.method == 'POST':
-        return updateOneSubject(request, id)
-    elif request.method == 'DELETE':
-        return deleteOneSubject(request, id)
-    else:
-        return JsonResponse({'message:': 'method not allowed'}, status=405)
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
